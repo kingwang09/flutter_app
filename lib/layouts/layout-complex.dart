13338505bc5d32b9e3e8274d89457d61399ced71
hyungeun <hyungeun.jin@wadiz.kr>
2023-01-09 15:11:13 +0900

    layout 최신화

--- lib/layouts/layout-complex.dart
+++ lib/layouts/layout-complex.dart
@@ -48,22 +48,81 @@ class MyApp extends StatelessWidget{
       ),
     );
 
-    final left = Column(
-      children: [
-        const Text("Title"),
-        const Text("여기는 내용 영역 입니다!"),
-        ratings,
-      ],
+    const descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18,
+      height: 2,
     );
 
-    return Row(
-      children: [
-        Center(
-          child: left,
+// DefaultTextStyle.merge() allows you to create a default text
+// style that is inherited by its child and all subsequent children.
+    final iconList = DefaultTextStyle.merge(
+      style: descTextStyle,
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Icon(Icons.kitchen, color: Colors.green[500]),
+                const Text('PREP:'),
+                const Text('25 min'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.timer, color: Colors.green[500]),
+                const Text('COOK:'),
+                const Text('1 hr'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.restaurant, color: Colors.green[500]),
+                const Text('FEEDS:'),
+                const Text('4-6'),
+              ],
+            ),
+          ],
         ),
-        Expanded(child: Image.asset("images/Lounge.jpg"))
-      ],
+      ),
     );
+
+    final leftColumn = Container(
+      padding: const EdgeInsets.fromLTRB(20, 30, 20, 20),
+      child: Column(
+        children: [
+          const Text("Title"),
+          const Text("여기는 서브 타이틀 내용 영역 입니다!"),
+          ratings,
+          iconList,
+        ],
+      ),
+    );
+
+    final main = Center(
+      child: Container(
+        margin: const EdgeInsets.fromLTRB(0, 40, 0, 30),
+        height: 600,
+        child: Card(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: 440,
+                child: leftColumn,
+              ),
+              Expanded(child: Image.asset("images/Lounge.jpg")),
+            ],
+          ),
+        ),
+      ),
+    );
+    return main;
   }
 
 }
